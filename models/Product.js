e2122692a9559919568d1a0dc4eeb756a468e973
bkness <kbrandon863@gmail.com>
2023-11-20 13:35:46 -0700

    added rules for product models

---
+++ models/Product.js
@@ -0,0 +1,25 @@
+// import important parts of sequelize library
+const { Model, DataTypes } = require('sequelize');
+// import our database connection from config.js
+const sequelize = require('../config/connection');
+
+// Initialize Product model (table) by extending off Sequelize's Model class
+class Product extends Model {}
+
+// set up fields and rules for Product model
+Product.init(
+  {
+    product_name:{type: DataTypes.STRING, allowNull: false},
+    stock:{type: DataTypes.INTEGER, defaultValue: 10, validate:{isNumeric: true}},
+     price:{type: DataTypes.DECIMAL, allowNull: false, validate:{isDecimal: true}} // allow null will kick the entry back to you if someething isnt defined 
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'product',
+  }
+);
+
+module.exports = Product;
